1dd2ceb20c7394c4887996995945dc4939addab1
georgikhristov <gh97@abv.bg>
2017-11-09 15:54:35 +0100

    Added function for gender in Calculator

--- app/src/main/java/ceci/viafitnessapp/Calculator.java
+++ app/src/main/java/ceci/viafitnessapp/Calculator.java
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class Calculator extends AppCompatActivity {
 
     EditText weight;
     EditText height;
     EditText age;
+    Switch gender;
     Button button;
     EditText result;
     double agee;
@@ -25,6 +29,7 @@ public class Calculator extends AppCompatActivity {
         age = (EditText) findViewById(R.id.editText3);
         button = (Button) findViewById(R.id.calculate);
         result = (EditText) findViewById(R.id.resultNumber);
+        gender = (Switch) findViewById(R.id.switch3);
 
     }
 
@@ -32,12 +37,16 @@ public class Calculator extends AppCompatActivity {
         if (weight.getText().toString().isEmpty() || height.getText().toString().isEmpty() || age.getText().toString().isEmpty()) {
 
         } else
-            agee = Integer.parseInt(age.getText().toString());
+                if(!gender.isChecked()) {
 
-        double bmi = 66.4730 + (13.7516 * Integer.valueOf(weight.getText().toString())) + (5.0033 * Integer.valueOf(height.getText().toString())) - (6.7550 * Integer.valueOf(age.getText().toString()));
+                    agee = Integer.parseInt(age.getText().toString());
+                    double bmi = 66.4730 + (13.7516 * Integer.valueOf(weight.getText().toString())) + (5.0033 * Integer.valueOf(height.getText().toString())) - (6.7550 * Integer.valueOf(age.getText().toString()));
+                    result.setText(String.valueOf(bmi));
+                }else{
+                    double bmi = 655.0955 + (9.5634 * Integer.valueOf(weight.getText().toString())) + (1.849 * Integer.valueOf(height.getText().toString())) - (4.6756  * Integer.valueOf(age.getText().toString()));
+                    result.setText(String.valueOf(bmi));
+                }
 
-        
-        result.setText(String.valueOf(bmi));
 
     }
 }
