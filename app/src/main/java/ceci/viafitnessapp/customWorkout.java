6640c73564152508961d183e4eba72aa9ddd4a14
georgikhristov <gh97@abv.bg>
2017-11-14 16:17:07 +0100

    Saving the value for the water conss

---
+++ app/src/main/java/ceci/viafitnessapp/customWorkout.java
@@ -0,0 +1,12 @@
+package ceci.viafitnessapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class customWorkout extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_workout);
+    }
+}
