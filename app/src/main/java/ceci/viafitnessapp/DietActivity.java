7b2319762c409026f4b2849d9e4fcca20fb7f9aa
CeciLD <33375854+CeciLD@users.noreply.github.com>
2017-11-14 16:14:53 +0100

    Diets List made "prettier"

--- app/src/main/java/ceci/viafitnessapp/DietActivity.java
+++ app/src/main/java/ceci/viafitnessapp/DietActivity.java
@@ -30,43 +30,33 @@ public class DietActivity extends AppCompatActivity {
         setContentView(R.layout.activity_diet);
         floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton2);
         data = new ArrayList<Diets>();
-        data.add(new Diets("The 4-week fat-burning meal plan", 7, "Fat Burning",R.drawable.a,"The get-lean grocery shopping list\n" +
+        data.add(new Diets("The 4-week fat-burning meal plan", 7, "Fat Burning",R.drawable.a,
+                "THE GET-LEAN GROCERY SHOPPING LIST\n" +
                 "\n" +
                 "Make sure your spice cabinet is stocked and go to the grocery store to get the following items:\n" +
                 "\n" +
-                "Plain Greek yogurt\n" +
-                "Fresh/frozen berries\n" +
-                "Other fresh fruit (bananas, grapefruit, apple etc)\n" +
-                "High-fiber high-protein breakfast cereal\n" +
-                "High-fiber/high-protein frozen waffles\n" +
-                "Natural peanut butter\n" +
-                "Eggs\n" +
-                "Low-fat/fat-free milk\n" +
-                "High-fiber/high-protein whole-grain bread\n" +
-                "High-fiber/high-protein whole-grain tortillas\n" +
-                "Large chicken breast\n" +
-                "Lean skirt steak\n" +
-                "Lean deli meat (turkey, roast beef)\n" +
-                "Ground lean turkey breast\n" +
-                "Canned tuna/salmon\n" +
-                "Fresh/frozen fish (salmon, tilapia etc.)\n" +
-                "Canned beans (black, lentil, pinto etc)\n" +
-                "Fresh/frozen vegetables\n" +
-                "Butternut squash\n" +
-                "Baked/sweet potato\n" +
-                "Avocado\n" +
-                "Low-fat cheese (sliced and shredded)\n" +
-                " \n" +
-                "CARB FOODS\n" +
-                "What you need to know about 'super starch'\n" +
-                "The new macro can help you burn more fat.\n" +
-                "The 4-week fat-burning workout plans \n" +
-                "\n" +
-                "The 6-day trim up plan\n" +
-                "The 5-day built for the beach plan\n" +
-                "The 3-day detox plan\n" +
-                "\n" +
-                "The 4-week fat-burning meal plan "));
+                "- Plain Greek yogurt\n" +
+                "- Fresh/frozen berries\n" +
+                "- Other fresh fruit (bananas, grapefruit, apple etc)\n" +
+                "- High-fiber high-protein breakfast cereal\n" +
+                "- High-fiber/high-protein frozen waffles\n" +
+                "- Natural peanut butter\n" +
+                "- Eggs\n" +
+                "- Low-fat/fat-free milk\n" +
+                "- High-fiber/high-protein whole-grain bread\n" +
+                "- High-fiber/high-protein whole-grain tortillas\n" +
+                "- Large chicken breast\n" +
+                "- Lean skirt steak\n" +
+                "- Lean deli meat (turkey, roast beef)\n" +
+                "- Ground lean turkey breast\n" +
+                "- Canned tuna/salmon\n" +
+                "- Fresh/frozen fish (salmon, tilapia etc.)\n" +
+                "- Canned beans (black, lentil, pinto etc)\n" +
+                "- Fresh/frozen vegetables\n" +
+                "- Butternut squash\n" +
+                "- Baked/sweet potato\n" +
+                "- Avocado\n" +
+                "- Low-fat cheese (sliced and shredded)\n"));
         data.add(new Diets("Test", 7, "test",R.drawable.ab,null));
         data.add(new Diets("Test", 7, "test",R.drawable.abc,null));
 
